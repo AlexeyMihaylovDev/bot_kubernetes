a5805c53750237311e903c602fdcf1b318204ee7
alexey <alexeymihaylovdev@gmail.com>
2022-11-23 15:11:11 +0200

    add jenkins agent build

---
+++ infra/jenkins/jenAgent/JenkinsAgent.Dockerfile
@@ -0,0 +1,75 @@
+FROM ubuntu as installer
+RUN apt-get update \
+  && apt-get install -y curl unzip
+
+RUN curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip \
+  && unzip awscliv2.zip \
+  && ./aws/install --bin-dir /aws-cli-bin/ \
+  && rm -rf aws awscliv2.zip
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    python3.5 \
+    python3-pip \
+    && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN pip3 install nibabel pydicom matplotlib pillow
+RUN pip3 install med2image
+
+
+RUN \
+# Update
+apt-get update -y && \
+# Install Unzip
+apt-get install unzip -y && \
+# need wget
+apt-get install wget -y && \
+# vim
+apt-get install vim -y
+# Download terraform for linux
+RUN wget https://releases.hashicorp.com/terraform/1.3.1/terraform_1.3.1_linux_amd64.zip
+# Unzip
+RUN unzip terraform_1.3.1_linux_amd64.zip
+
+# Maven
+# Setting Maven Version that needs to be installed
+ARG MAVEN_VERSION=3.8.6
+RUN curl -fsSL https://archive.apache.org/dist/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz | tar xzf - -C /usr/share \
+  && mv /usr/share/apache-maven-$MAVEN_VERSION /usr/share/maven \
+  && ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
+
+
+RUN curl -s -L https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747.zip -o sonarscanner.zip \
+  && unzip -qq sonarscanner.zip \
+  && rm -rf sonarscanner.zip \
+  && mv sonar-scanner-4.7.0.2747 sonar-scanner
+
+# kubectl
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
+    && chmod +x kubectl \
+    && mv ./kubectl /usr/local/bin/
+
+
+FROM jenkins/agent
+COPY --from=docker /usr/local/bin/docker /usr/local/bin/
+COPY --from=installer /usr/local/aws-cli/ /usr/local/aws-cli/
+COPY --from=installer /aws-cli-bin/ /usr/local/bin/
+COPY --from=installer terraform /usr/local/bin/
+COPY --from=installer /usr/share/maven /usr/share/maven
+COPY --from=installer /usr/share/maven/bin/mvn /usr/bin/mvn
+COPY --from=installer sonar-scanner /usr/local/sonar-scanner/
+COPY --from=installer sonar-scanner/bin/ /usr/local/bin/
+COPY --from=installer sonar-scanner/conf/sonar-scanner.properties  /usr/local/sonar-scanner/conf/sonar-scanner.properties
+COPY --from=installer /usr/local/bin/kubectl /usr/local/bin/
+
+
+ENV SONAR_RUNNER_HOME=/usr/local/sonar-scanner/
+ENV SONAR $PATH:$SONAR_RUNNER_HOME/bin
+ENV PATH $SONAR_RUNNER_HOME:$PATH
+
+ENV MAVEN_VERSION=${MAVEN_VERSION}
+ENV M2_HOME /usr/share/maven
+ENV maven.home $M2_HOME
+ENV M2 $M2_HOME/bin
+ENV PATH $M2:$PATH
---
+++ infra/jenkins/jenAgent/build.groovy
@@ -0,0 +1,90 @@
+@Library('global_jenkins_functions') _
+
+
+import groovy.time.TimeDuration
+import groovy.transform.Field
+/*************************************************************** | PARAMETERS | ******************************************************************/
+
+// GLOABAL PARAMETERS
+
+@Field JOB = [:]
+
+JOB.docker_file_path = "infra/jenkins/jenAgent/JenkinsAgent.Dockerfile"
+JOB.git_project_url = "https://github.com/AlexeyMihaylovDev/bot_kubernetes.git"
+JOB.project_name = "JenkinsAgent"
+JOB.devops_sys_user = "my_polybot_key"
+JOB.branch = "feature/deploy"
+JOB.email_recepients = "mamtata2022@gmail.com"
+JOB.user_run = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
+
+pipeline {
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '30', artifactNumToKeepStr: '30'))
+        timestamps()
+        ansiColor('xterm')
+    }
+    agent { label 'linux' }
+    environment {
+        REGISTRY_URL = "352708296901.dkr.ecr.eu-central-1.amazonaws.com"
+        REGISTRY_REGION = "eu-central-1"
+        BOT_ECR_NAME = "alexey_jenk_agent"
+        IMAGE_ID = "${env.REGISTRY_URL}/alexey_jenk_agent"
+    }
+
+    stages {
+        stage('Get & Print Job Parameters') {
+            steps {
+                script {
+                    println("======================== print all jobs properties ===================")
+                    JOB.each { key, value ->
+                        println(' ' + key + '==' + ' ' + value)
+                    }
+
+                }
+            }
+        }
+        stage('Clone') {
+            steps {
+                script {
+                    // Clone PolyBot repository.
+                    git branch: "${JOB.branch}", url: "${JOB.git_project_url}"
+                    JOB.gitCommitHash = global_gitInfo.getCommitHash(JOB.branch)
+                    println("====================${JOB.gitCommitHash}==============")
+                }
+            }
+        }
+        stage("build") {
+            steps {
+                sh "aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $REGISTRY_URL"
+                script {
+                            def imageName = "$BOT_ECR_NAME:${env.BUILD_NUMBER}"
+                            def  finalImageName = "$REGISTRY_URL/$BOT_ECR_NAME:${env.BUILD_NUMBER}"
+                            sh "docker build -t $imageName -f  ${JOB.modules[moduleName]['dockerfile']} ."
+                            sh "docker tag $imageName $finalImageName"
+                            sh "docker push $finalImageName"
+                        }
+                    }
+
+                }
+            }
+    post {
+
+        always {
+            script {
+                currentBuild.description = ("Branch : ${JOB.branch}\n GitCommiter : ${JOB.commitAuthor}\nDeploy_server: ${JOB.deploy}")
+
+                EMAIL_MAP = [
+                        "Job Name"      : JOB_NAME,
+                        "Build Number"  : BUILD_NUMBER,
+                        "Git tag Name"  : JOB.tagName,
+                        "Branch"        : "${JOB.branch}",
+                        "More Info At"  : "<a href=${BUILD_URL}console> Click here to view build console on Jenkins. </a>",
+                        "painted"       : "false"
+                ]
+                global_sendGlobalMail.sendByMapFormat(JOB.email_recepients, currentBuild.result, EMAIL_MAP,
+                        "Jenkins Report", "Build Notification - Jenkins Report", "BOT build")
+
+            }
+        }
+    }
+}
