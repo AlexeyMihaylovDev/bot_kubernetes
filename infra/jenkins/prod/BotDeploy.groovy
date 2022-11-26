86de1816f071536bcf4c437ad25a402ff3544a19
alexey <alexeymihaylovdev@gmail.com>
2022-11-26 12:07:31 +0200

    check

---
+++ infra/jenkins/prod/BotDeploy.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent {
+        docker {
+            label 'kuber'
+            image '352708296901.dkr.ecr.eu-central-1.amazonaws.com/alexey_jenk_agent:7'
+            args '--user root -v /var/run/docker.sock:/var/run/docker.sock'
+        }
+    }
+    environment {
+        APP_ENV = "prod"
+    }
+
+//    parameters {
+//        string(name: 'BOT_IMAGE_NAME')
+//    }
+
+    stages {
+        stage('Bot Deploy') {
+            steps {
+                withCredentials([
+                        string(credentialsId: 'telegram-bot-token', variable: 'TELEGRAM_TOKEN'), //TODO change telegram token to prod
+                        file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')
+                ]) {
+                    sh '''
+                    K8S_CONFIGS=infra/k8s
+
+                    # replace placeholders in YAML k8s files
+                    bash common/replaceInFile.sh $K8S_CONFIGS/bot.yaml APP_ENV $APP_ENV
+                    bash common/replaceInFile.sh $K8S_CONFIGS/bot.yaml BOT_IMAGE $BOT_IMAGE_NAME
+                    bash common/replaceInFile.sh $K8S_CONFIGS/bot.yaml TELEGRAM_TOKEN $(echo $TELEGRAM_TOKEN | base64)
+
+                    # apply the configurations to k8s cluster
+                    kubectl apply --kubeconfig ${KUBECONFIG} -f $K8S_CONFIGS/bot.yaml
+                    '''
+                }
+            }
+        }
+    }
+}
