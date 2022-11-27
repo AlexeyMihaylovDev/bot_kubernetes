08127472b0a1b5ebed6106b870f797fe6f23d456
DevOps <alexeymihaylovdev@gmail.com>
2022-11-27 10:36:58 +0200

    add prod pipelines

--- infra/jenkins/prod/BotDeploy.groovy
+++ infra/jenkins/prod/BotDeploy.groovy
@@ -24,7 +24,7 @@ pipeline {
                 script {
 
                     sh "aws ecr list-images --repository-name alexey_bot_prod | jq \'.imageIds[] | .imageTag\' > images.txt"
-                    JOB.images = readFile("${env.WORKSPACE}/images.txt").replace("\"", "").split("\n") as List
+                    JOB.images = readFile("${env.WORKSPACE}/images.txt").replace("\"", "").split("\\s*") as List
                     println(JOB.images)
 
                 }
